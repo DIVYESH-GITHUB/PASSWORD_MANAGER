cebbf3f77fbf9bf174e68baa2618ae57b13edde8
DIVYESH-GITHUB <divyeshpindaria09@gmail.com>
2024-01-18 13:05:55 +0530

    API ERROR AND RESPONSE UTILITIES

---
+++ server/src/utils/apiError.js
@@ -0,0 +1,9 @@
+class ApiError {
+  constructor(statusCode, success = false, errorMessage) {
+    this.statusCode = statusCode;
+    this.success = success;
+    this.errorMessage = errorMessage;
+  }
+}
+
+export { ApiError };
